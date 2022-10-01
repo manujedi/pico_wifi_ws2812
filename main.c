08804e1a85ee10fc9259ce2c92c6f77b2e878ea7
Manuel Jedinger <manuel.jedinger@student.tugraz.at>
2022-10-01 17:12:04 +0200

    pico only version

--- main.c
+++ main.c
@@ -7,14 +7,9 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <pico/printf.h>
 
 #include "pico/stdlib.h"
-#include "pico/cyw43_arch.h"
-
-#include "lwip/pbuf.h"
-#include "lwip/tcp.h"
-
-#include "tcpserver.h"
 
 #include "hardware/pio.h"
 #include "hardware/clocks.h"
@@ -23,101 +18,137 @@
 
 #include "pico/multicore.h"
 
+
 #define LEDS 16
-#define LINES 4
+#define LINES 6
 
 uint32_t leds[LINES][LEDS];
 
+volatile uint32_t refreshrate = 0;
 
-void setLed(uint16_t  line, uint16_t led, uint8_t red, uint8_t green, uint8_t blue){
-    leds[line][led] = (red<<16) | (green << 24) | (blue << 8);
+
+void setLed(uint16_t line, uint16_t led, uint8_t red, uint8_t green, uint8_t blue) {
+    leds[line][led] = (red << 16) | (green << 24) | (blue << 8);
 
 }
 
-void writeLeds(){
+
+void writeLeds() {
+
+    //write all
     for (int j = 0; j < LEDS; j++) {
         pio_sm_put_blocking(pio0, 0, leds[0][j]);
         pio_sm_put_blocking(pio0, 1, leds[1][j]);
         pio_sm_put_blocking(pio0, 2, leds[2][j]);
         pio_sm_put_blocking(pio0, 3, leds[3][j]);
+        pio_sm_put_blocking(pio1, 0, leds[4][j]);
+        pio_sm_put_blocking(pio1, 1, leds[5][j]);
     }
+
 }
 
-
+bool timer_callback(struct repeating_timer *t) {
+    refreshrate++;
+    writeLeds();
+    return true;
+}
 
 void core1_entry() {
+
+    uint8_t red = 0;
+    uint8_t green = 0;
+    uint8_t blue = 0;
+    uint8_t state = 0;
+
     while (1) {
 
-        writeLeds();
-        sleep_ms(10);
+        switch (state) {
+            //from red to green
+            case 0:
+                red--;
+                green++;
+                if (green == 255) {
+                    red = 0;
+                    state = 1;
+                }
+                break;
+            case 1:
+                //green to blue
+                green--;
+                blue++;
+                if (blue == 255) {
+                    green = 0;
+                    state = 2;
+                }
+                break;
+            case 2:
+                //blue to red
+                blue--;
+                red++;
+                if (red == 255) {
+                    blue = 0;
+                    state = 0;
+                }
+                break;
+            default:
+                break;
+        }
+
+        setLed(0, 0, red, green, blue);
+
+
+
+        for (int line = LINES-1; line >= 0; line--) {
+            for (int led = LEDS-1; led >= 0; led--) {
+                if(led == 0 && line != 0) {
+                    leds[line][0] = leds[line-1][LEDS-1];
+                    continue;
+                }
+
+                if(led == 0)
+                    continue;
+
+                leds[line][led] = leds[line][led-1];
+            }
+        }
+        sleep_ms(1000);
     }
+
+
+
 }
 
 
 int main() {
-    stdio_init_all();
+    stdio_init_all();;
+    uint offset0 = pio_add_program(pio0, &ws2812_program);
+    ws2812_program_init(pio0, 0, offset0, 21, 800000, 0);
+    ws2812_program_init(pio0, 1, offset0, 20, 800000, 0);
+    ws2812_program_init(pio0, 2, offset0, 19, 800000, 0);
+    ws2812_program_init(pio0, 3, offset0, 18, 800000, 0);
 
-    PIO pio = pio0;
-    uint offset = pio_add_program(pio, &ws2812_program);
-    for (int i = 0; i < 4; ++i) {
-        ws2812_program_init(pio, i, offset, i+2, 800000, 0);
-    }
+    uint offset1 = pio_add_program(pio1, &ws2812_program);
+    ws2812_program_init(pio1, 0, offset1, 17, 800000, 0);
+    ws2812_program_init(pio1, 1, offset1, 16, 800000, 0);
 
 
-    if (cyw43_arch_init()) {
-        printf("failed to initialise\n");
-        return 1;
-    }
+    setLed(LINES - 1, LEDS - 1, 0xf, 0xf, 0xf);
 
-    cyw43_arch_enable_sta_mode();
+    struct repeating_timer timer;
+    add_repeating_timer_ms(-1000, timer_callback, NULL, &timer); //120HZ
 
-    printf("Connecting to WiFi...\n");
-    if (cyw43_arch_wifi_connect_timeout_ms(WIFI_SSID, WIFI_PASSWORD, CYW43_AUTH_WPA2_AES_PSK, 30000)) {
-        printf("failed to connect.\n");
-        return 1;
-    } else {
-        printf("Connected.\n");
-    }
-
-    uint8_t red=0;
-    uint8_t green=0;
-    uint8_t blue=0;
-    uint16_t i=0;
-
-    rgb = 0x000001;
     multicore_launch_core1(core1_entry);
 
-    __attribute__((unused)) struct tcp_pcb *server_pcb = tcp_serveropen();
-    while(1){
-        printf("alive... %i connected\n", currently_connected);
 
-        red = rgb >> 16;
-        green = (rgb >> 8) & 0xFF;
-        blue = rgb & 0xFF;
-
-        printf("LED: R: %x, G: %x, B: %x\n",red,green,blue);
-
-        setLed(0, 0,red,green,blue);
-
-        for (int j = 1; j < LINES; ++j) {
-            leds[j][0] = leds[j-1][LEDS-1];
-        }
-        for (int j = 0; j <LINES; ++j) {
-            i=LEDS;
-            while(i > 1){
-                i--;
-                leds[j][i] = leds[j][i-1];
-            }
-        }
-
-
-
+    while (1) {
+        printf("refreshrate: %iHz\n", refreshrate);
+        refreshrate = 0;
         sleep_ms(1000);
 
     }
-    tcp_server_close(server_pcb);
 
 
-    cyw43_arch_deinit();
+
+
     return 0;
 }
